9f6c0b4ea468391cf3de46c7d64fa94d16f1f07a
Murat karabulut <53555874+Guru444@users.noreply.github.com>
2020-02-29 00:46:04 +0300

    Update chat.php

--- chat.php
+++ chat.php
@@ -305,22 +305,3 @@ input[type=button]{
 
 
 </style>
-
-
-<?php include('footer.php'); ?>
-	<!-- jQuery -->
-
-	<script src="js/jquery.min.js"></script>
-	<!-- jQuery Easing -->
-	<script src="js/jquery.easing.1.3.js"></script>
-	<!-- Bootstrap -->
-	<script src="js/bootstrap.min.js"></script>
-	<!-- Waypoints -->
-	<script src="js/jquery.waypoints.min.js"></script>
-	<!-- Superfish -->
-	<script src="js/hoverIntent.js"></script>
-	<script src="js/superfish.js"></script>
-	<!-- Flexslider -->
-	<script src="js/jquery.flexslider-min.js"></script>
-	<!-- Main JS (Do not remove) -->
-	<script src="js/main.js"></script>
